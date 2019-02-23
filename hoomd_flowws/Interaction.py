f3d59f5599284335528e8211a546fbafb9d8dc92
Matthew Spellings <mspells@umich.edu>
2019-02-22 20:23:39 -0500

    Improve command-line parsing on Interaction

--- hoomd_flowws/Interaction.py
+++ hoomd_flowws/Interaction.py
@@ -1,3 +1,4 @@
+import argparse
 import collections
 import itertools
 
@@ -23,6 +24,41 @@ class Interaction(flowws.Stage):
             callbacks['pre_run'] = pre_run_callbacks
 
         callbacks['pre_run'].append(self)
+
+    @classmethod
+    def from_command(cls, args):
+        parser = argparse.ArgumentParser(
+            prog=cls.__name__, description=cls.__doc__)
+
+        parser.add_argument('--reset', default=False, action='store_true',
+            help=cls.ARGS[0].description)
+        parser.add_argument('--type', required=True,
+            help=cls.ARGS[1].description)
+        parser.add_argument('-g', '--global-params', nargs=2, action='append', default=[],
+            metavar=('name', 'value'),
+            help='Set global parameters of the interaction')
+        parser.add_argument('-p', '--pair-params', nargs=4, action='append', default=[],
+            metavar=('A', 'B', 'name', 'value'),
+            help='Set type-pair interaction parameters (_ for all types)')
+
+        args = parser.parse_args(args)
+
+        pair_params = collections.defaultdict(dict)
+        for (a, b, name, value) in args.pair_params:
+            pair_params[(a, b)][name] = eval(value)
+
+        pair_params_list = []
+        for (a, b, _, _) in args.pair_params:
+            if (a, b) in pair_params:
+                element = (a, b, pair_params.pop((a, b)))
+                pair_params_list.append(element)
+
+        potential_params = dict(pair_params=pair_params_list)
+
+        for (name, value) in args.global_params:
+            potential_params[name] = eval(value)
+
+        return cls(reset=args.reset, type=args.type, params=potential_params)
 
     def __call__(self, scope, storage):
         interaction_type = self.arguments['type']
