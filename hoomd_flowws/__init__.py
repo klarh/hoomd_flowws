b3fa5ae653e3ff475b08c5556e4d13f05afff4b6
Matthew Spellings <mspells@umich.edu>
2020-02-06 00:22:39 -0500

    Use flowws.try_to_import to expose all modules

--- hoomd_flowws/__init__.py
+++ hoomd_flowws/__init__.py
@@ -1,5 +1,11 @@
+from flowws import try_to_import
+
 from .version import __version__
 
-from .Interaction import Interaction
-from .Run import Run
-from .ShapeDefinition import ShapeDefinition
+Damasceno2017Interaction = try_to_import('.Damasceno2017Interaction', 'Damasceno2017Interaction', __name__)
+DEMInteraction = try_to_import('.DEMInteraction', 'DEMInteraction', __name__)
+Init = try_to_import('.Init', 'Init', __name__)
+Interaction = try_to_import('.Interaction', 'Interaction', __name__)
+Run = try_to_import('.Run', 'Run', __name__)
+RunHPMC = try_to_import('.RunHPMC', 'RunHPMC', __name__)
+ShapeDefinition = try_to_import('.ShapeDefinition', 'ShapeDefinition', __name__)
