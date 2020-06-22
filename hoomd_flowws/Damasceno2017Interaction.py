Fix sign problem in Damasceno2017Interaction
The sign of the epsilon-multiplied portion of the potential was
changed between the arxiv preprint and the final published version of
the paper. This change notes that difference and makes things
consistent with the published version.

hoomd_flowws/Damasceno2017Interaction.py
@@ -54,7 +54,7 @@ class Damasceno2017Interaction(flowws.Stage):
         # subtract 1 to change index from from counting numbers (as
         # reported in the paper) to list indices (as used in this
         # module)
-        potential_index = int(round((2.2 - self.arguments['depth'])/0.4)) - 1
+        potential_index = int(round((2.2 + self.arguments['depth'])/0.4)) - 1
         if potential_index < 0 or potential_index >= len(SI_TABLE_POTENTIALS):
             logger.warning('Potential well depth out of range, using the closest one in range instead')
         potential_index = np.clip(potential_index, 0, 9)
@@ -79,10 +79,10 @@ class Damasceno2017Interaction(flowws.Stage):
             func=local_table_grabber, rmin=rmin, rmax=rmax, coeff={})
 
 # contents of "potential.01.dat" through "potential.10.dat" from the
-# SI. Note that the paper says the well depth varies as -2.2 + 0.4*n,
-# but upon plotting you can see that "potential.01.dat" is the most
-# positive, rather than the most negative, potential in the
-# series. Each row contains "r, energy, force" data.
+# SI. Note that the well depth varies as -2.2 + 0.4*n and that the
+# epsilon term is *subtracted* from the potential, not added (see the
+# final published version of the paper, not the arxiv preprint). Each
+# row contains "r, energy, force" data.
 SI_TABLE_POTENTIALS = []
 
 SI_TABLE_POTENTIALS.append(