1edaf304e6cf82d308a4f964cf2602a213aae86b
Matthew Spellings <mspells@vectorinstitute.ai>
2021-06-10 15:19:44 -0400

    Add Dshemuchadse2021 interactions to default package imports

--- hoomd_flowws/__init__.py
+++ hoomd_flowws/__init__.py
@@ -4,6 +4,10 @@ from .version import __version__
 
 Damasceno2017Interaction = try_to_import('.Damasceno2017Interaction', 'Damasceno2017Interaction', __name__)
 DEMInteraction = try_to_import('.DEMInteraction', 'DEMInteraction', __name__)
+Dshemuchadse2021LJGInteraction = try_to_import(
+    '.Dshemuchadse2021LJGInteraction', 'Dshemuchadse2021LJGInteraction', __name__)
+Dshemuchadse2021OPPInteraction = try_to_import(
+    '.Dshemuchadse2021OPPInteraction', 'Dshemuchadse2021OPPInteraction', __name__)
 Init = try_to_import('.Init', 'Init', __name__)
 Interaction = try_to_import('.Interaction', 'Interaction', __name__)
 Run = try_to_import('.Run', 'Run', __name__)
